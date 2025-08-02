df491e582bc31439eeb92c2304bb2aeb4054c487
s0obang <tnqls40168@gmail.com>
2025-08-02 21:28:22 +0900

    [Setting]first commit

---
+++ src/main/java/com/example/centralhackathon/CentralHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.centralhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/centralhackathon/CentralHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centralhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
