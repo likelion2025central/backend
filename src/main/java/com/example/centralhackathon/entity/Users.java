b801967a3ba7cd1117de2bd2dfe6fd7bcc22d0ac
s0obang <tnqls40168@gmail.com>
2025-08-23 21:56:23 +0900

    [Feat/#10] 일반학생

--- src/main/java/com/example/centralhackathon/entity/Users.java
+++ src/main/java/com/example/centralhackathon/entity/Users.java
@@ -7,14 +7,13 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(name="uk_users_username", columnNames = "username")
-})
+@Table(name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "role", length = 20) // 'STUDENT','COUNCIL','BOSS'
 public abstract class Users extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String username;
     @Column(nullable = false)
     private String password;
